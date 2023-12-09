3017e116289ee64b59c10403e94ad08f0b8b4906
ahmedmehmood5147 <am9474036@gmail.com>
2023-12-10 01:55:47 +0500

    assignment 4

---
+++ app.js
@@ -0,0 +1,23 @@
+//answer 1
+var a = 5;
+var b = "ahmed"
+var bollean = true
+//answer 2
+//Legal variable names
+//var ahmed
+// var count
+// var mehmood
+//var ayaz
+//var umair
+//illegal variable names
+//12ahmed
+//user name
+//variable
+//break
+//class
+//answer 3
+var heading = "Rules for naming JS variables"
+document.write(heading+ "<br>")
+document.write("variable names can only contain numbers $ "+ "<br>")
+document.write("variable names are case sensitive"+ "<br>")
+document.write("variable names should not be Js keywords "+ "<br>")
